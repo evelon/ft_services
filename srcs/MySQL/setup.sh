7423b6ac71128f07023e51a5af32b3569ad89730
jolim <jolim@student.42seoul.fr>
2021-03-30 19:37:51 +0900

    nginx image problem solved. pma touched

--- srcs/MySQL/setup.sh
+++ srcs/MySQL/setup.sh
@@ -1,3 +1,3 @@
-#!bin/bash
+#!/bin/bash
 docker image rm -f alpine_mysql:service
 docker build -t alpine_mysql:service $(dirname "$0")
--- srcs/nginx/build.sh
+++ srcs/nginx/build.sh
@@ -1,4 +1,4 @@
-#!bin/bash
+#!/bin/bash
 BASEDIR=$(dirname "$0")
 docker image rm -f alpine_nginx:service
 docker build -t alpine_nginx:service $BASEDIR
